593c31ffd468b730d521021e336179cbf7b1a526
chandhru <chandhru.ravi@bunch.capital>
2026-06-07 00:48:21 +0200

    refactor: replace custom JwtValidator with authaction-ruby-sdk

--- Gemfile
+++ Gemfile
@@ -3,5 +3,5 @@ source "https://rubygems.org"
 ruby ">= 3.2"
 
 gem "rails", "~> 7.2"
-gem "jwt", "~> 2.9"
+gem "authaction-ruby-sdk"
 gem "dotenv-rails", groups: [:development, :test]
--- app/controllers/concerns/jwt_authenticatable.rb
+++ app/controllers/concerns/jwt_authenticatable.rb
@@ -1,23 +1,6 @@
-require "jwt_validator"
+require "authaction/rails"
 
 module JwtAuthenticatable
   extend ActiveSupport::Concern
-
-  private
-
-  def authenticate_request!
-    token = extract_bearer_token
-    @current_payload = JwtValidator.verify(token)
-  rescue JWT::ExpiredSignature
-    render json: { error: "Token has expired" }, status: :unauthorized
-  rescue JWT::DecodeError => e
-    render json: { error: e.message }, status: :unauthorized
-  end
-
-  def extract_bearer_token
-    header = request.headers["Authorization"]
-    raise JWT::DecodeError, "Missing token" unless header&.start_with?("Bearer ")
-
-    header.split(" ", 2).last.strip
-  end
+  include AuthAction::Rails::JwtAuthenticatable
 end
